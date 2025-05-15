a957ed1f4e333d11d7ddf40b2af307b42492c9dc
mariam175 <mariamrafaat28@gmail.com>
2025-05-16 00:54:19 +0300

    SportsScreen

---
+++ Sports/View/SportsScreen/SportCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  SportCollectionViewCell.swift
+//  TestSport
+//
+//  Created by Macos on 14/05/2025.
+//
+
+import UIKit
+
+class SportCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var sportName: UILabel!
+    
+    @IBOutlet weak var sportImage: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
