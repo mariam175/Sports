940e022d824a7c0af1a685379d85ae33401bb3bf
MohammedHussien10 <mohammedhussien10101010@gmail.com>
2025-05-23 23:09:52 +0300

    add Unit Testing

---
+++ Sports/Model/Services/NetworkObserver.swift
@@ -0,0 +1,48 @@
+//
+//  NetworkObserver.swift
+//  Sports
+//
+//  Created by Macos on 22/05/2025.
+//
+
+import Foundation
+import Reachability
+class NetworkObserver {
+    private var reachability: Reachability?
+    weak var delegate: NetworkStatusDelegate?
+
+    init(delegate: NetworkStatusDelegate?) {
+        self.delegate = delegate
+        setupReachability()
+    }
+
+    private func setupReachability() {
+        do {
+            reachability = try Reachability()
+            NotificationCenter.default.addObserver(
+                self,
+                selector: #selector(reachabilityChanged),
+                name: .reachabilityChanged,
+                object: nil
+            )
+
+            try reachability?.startNotifier()
+        } catch {
+            print("Unable to start notifier")
+        }
+    }
+
+    @objc private func reachabilityChanged(note: Notification) {
+        guard let reachability = note.object as? Reachability else { return }
+        let isConnected = reachability.connection != .unavailable
+        delegate?.networkStatusChanged(isConnected: isConnected)
+    }
+
+    func isConnectedToInternet() -> Bool {
+        return reachability?.connection != .unavailable
+    }
+    deinit {
+        reachability?.stopNotifier()
+        NotificationCenter.default.removeObserver(self, name: .reachabilityChanged, object: reachability)
+    }
+}
